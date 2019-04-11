8f6e0f4c966cde9e4ec2fdcf99c43fb53e5e3b14
Jean-Francois Larouche <rouche@gmail.com>
2019-04-11 16:02:35 -0400

    Common Testing Frameworks

---
+++ tdd-by-example/src/main/java/guru/springframework/Foo.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+/**
+ * @Author Jean-Francois Larouche (jealar2) on 2019-04-11
+ */
+public class Foo {
+
+    public String getBar() {
+        return "Foobar";
+    }
+}
