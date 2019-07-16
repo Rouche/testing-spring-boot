bee36c2f3e4afbe55699706b794f59e0e46d29f4
Jean-Francois Larouche <rouche@gmail.com>
2019-07-16 13:43:27 -0400

    JUnit Dependants Assertions

---
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package org.kitfox.springboot.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OwnerTest {
+
+    @Test
+    void dependantAssertions() {
+        Owner owner = new Owner(1L, "Joe", "Shmoe");
+        owner.setCity("Vancouver");
+        owner.setTelephone("5551231212");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did Not Match"),
+                        () -> assertEquals("Shmoe", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Vancouver", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("5551231212", owner.getTelephone())
+                ));
+    }
+}
