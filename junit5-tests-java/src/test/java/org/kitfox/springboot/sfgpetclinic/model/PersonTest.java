83f84f829b3c86e0eee070991aed6019514b8681
Jean-Francois Larouche <rouche@gmail.com>
2019-07-16 13:33:18 -0400

    JUnit Grouped Assertions

---
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,31 @@
+package org.kitfox.springboot.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        //given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck"));
+    }
+
+    @Test
+    void groupedAssertionsMsgs() {
+        //given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        //then
+        assertAll("Test Props Set Msgs",
+                () -> assertEquals(person.getFirstName(), "Joe", "First name failed"),
+                () -> assertEquals(person.getLastName(), "Buck"));
+    }
+}
