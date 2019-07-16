2cdaa25f80425f7791cb86f8a58554d33dbac798
Jean-Francois Larouche <rouche@gmail.com>
2019-07-16 14:15:00 -0400

    Display names

--- junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/controllers/IndexControllerTest.java
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,9 +1,11 @@
 package org.kitfox.springboot.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IndexControllerTest {
 
@@ -14,12 +16,14 @@ class IndexControllerTest {
         indexController = new IndexController();
     }
 
+    @DisplayName("Test proper view name returnde for index page")
     @Test
     void index() {
         assertEquals("index", indexController.index());
     }
 
     @Test
+    @DisplayName("Test Exception different annotation order")
     void oupsHandler() {
         assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is some expensive" +
                 " message to build" + " during test");
