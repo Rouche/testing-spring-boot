1a38966d13520318ecdf12b61df6ac73361e7172
Rouche <rouche@gmail.com>
2019-12-03 10:40:36 -0500

    JUnit Parameterized tests - CSV source

--- junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/OwnerTest.java
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,7 @@ package org.kitfox.springboot.sfgpetclinic.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.kitfox.springboot.sfgpetclinic.ModelTests;
@@ -44,5 +45,12 @@ class OwnerTest implements ModelTests {
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV input Test")
+    @ParameterizedTest(name = "{displayName} => [{index}] {arguments}")
+    @CsvSource({"FL, 1, 2", "QC, 2, 3", "ON, 4, 5"})
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " " + val1 + " " + val2);
     }
 }
