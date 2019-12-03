28cc8c52dba3d196c3222a3ea6ff1beed3220905
Rouche <rouche@gmail.com>
2019-12-03 10:45:59 -0500

    JUnit Parameterized tests - CSV File source

--- junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/OwnerTest.java
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,7 @@ package org.kitfox.springboot.sfgpetclinic.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -51,6 +52,13 @@ class OwnerTest implements ModelTests {
     @ParameterizedTest(name = "{displayName} => [{index}] {arguments}")
     @CsvSource({"FL, 1, 2", "QC, 2, 3", "ON, 4, 5"})
     void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " " + val1 + " " + val2);
+    }
+
+    @DisplayName("CSV File input Test")
+    @ParameterizedTest(name = "{displayName} => [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFileInputTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " " + val1 + " " + val2);
     }
 }
