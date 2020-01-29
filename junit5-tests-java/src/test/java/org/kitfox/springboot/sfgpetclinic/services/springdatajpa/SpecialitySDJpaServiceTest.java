abe70523e973aa73c27b8fe87380ddaecceb2848
Rouche <rouche@gmail.com>
2020-01-29 15:47:46 -0500

    Returning values from Mocks

--- junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,5 +1,7 @@
 package org.kitfox.springboot.sfgpetclinic.services.springdatajpa;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kitfox.springboot.sfgpetclinic.model.Speciality;
@@ -8,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,5 +48,17 @@ class SpecialitySDJpaServiceTest {
     @Test
     void delete() {
         specialitySDJpaService.delete(new Speciality());
+    }
+
+    @Test
+    void findById() {
+        Speciality speciality = new Speciality();
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality found = specialitySDJpaService.findById(1L);
+
+        assertThat(found).isNotNull();
+
+        verify(specialtyRepository).findById(1L);
     }
 }
