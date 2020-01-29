4cea4a290c5d8acca704a7a830bed75e4a85ca46
Rouche <rouche@gmail.com>
2020-01-29 15:35:45 -0500

    Verify interactions with Mockito mock

--- junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,7 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -20,8 +20,26 @@ class SpecialitySDJpaServiceTest {
     private SpecialitySDJpaService specialitySDJpaService;
 
     @Test
-    void deleteById() {
+    void deleteByIdAtLeast() {
         specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
+    }
+    @Test
+    void deleteByTimes() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        verify(specialtyRepository, times(2)).deleteById(1L);
     }
 
     @Test
