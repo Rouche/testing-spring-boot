fe9fbc697de9e89c87eec925ed4d949397ee4910
Rouche <rouche@gmail.com>
2020-02-14 15:34:22 -0500

    Mockito Spies

---
+++ junit5-tests-java/src/test/java/org/kitfox/springboot/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,79 @@
+package org.kitfox.springboot.sfgpetclinic.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kitfox.springboot.sfgpetclinic.model.Pet;
+import org.kitfox.springboot.sfgpetclinic.model.Visit;
+import org.kitfox.springboot.sfgpetclinic.services.VisitService;
+import org.kitfox.springboot.sfgpetclinic.services.map.PetMapService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+
+    @Mock
+    VisitService visitService;
+
+    @Spy //@Mock
+    PetMapService petService;
+
+    @InjectMocks
+    VisitController visitController;
+
+    @Test
+    void loadPetWithVisit() {
+        //given
+        Map<String, Object> model = new HashMap<>();
+        Pet pet = new Pet(12L);
+        Pet pet3 = new Pet(3L);
+
+        petService.save(pet);
+        petService.save(pet3);
+
+        given(petService.findById(anyLong())).willCallRealMethod(); //.willReturn(pet);
+
+        //when
+        Visit visit = visitController.loadPetWithVisit(12L, model);
+
+        //then
+        assertThat(visit).isNotNull();
+        assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isEqualTo(12L);
+        verify(petService, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void loadPetWithVisitWithStubbing() {
+        //given
+        Map<String, Object> model = new HashMap<>();
+        Pet pet = new Pet(12L);
+        Pet pet3 = new Pet(3L);
+
+        petService.save(pet);
+        petService.save(pet3);
+
+        given(petService.findById(anyLong())).willReturn(pet3);
+
+        //when
+        Visit visit = visitController.loadPetWithVisit(12L, model);
+
+        //then
+        assertThat(visit).isNotNull();
+        assertThat(visit.getPet()).isNotNull();
+        assertThat(visit.getPet().getId()).isEqualTo(3L);
+        verify(petService, times(1)).findById(anyLong());
+    }
+
+}
