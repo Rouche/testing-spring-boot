ab7e11d36ed796c78e50bd5667f1eeeae070d088
Jean-Francois Larouche <rouche@gmail.com>
2020-04-16 21:43:42 -0400

    Using component scans

---
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterTest.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringJUnitConfig(classes = {HearingInterpreterInnerClassTest.TestConfig.class})
+class HearingInterpreterTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
+
+    @Autowired
+    private HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals(word, "Laurel");
+    }
+}
