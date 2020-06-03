9869d6518b97af072d61e5bbfaf68e14e7623f1f
Jean-Francois Larouche <rouche@gmail.com>
2020-06-03 14:52:30 -0400

    Assingment Assignment - Complete Tests for Find Form Review

--- tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,20 +1,29 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.reset;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ExtendWith(MockitoExtension.class) // ROUCHE_DOCS: For Captor
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
@@ -24,17 +33,28 @@ class OwnerControllerTest {
     @Autowired
     private ClinicService clinicService;
 
+    @Captor
+    private ArgumentCaptor<String> stringCaptor;
+
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        given(clinicService.findOwnerByLastName(anyString())).willReturn(new ArrayList<>());
-
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    /**
+     * ROUCHE_DOCS: This is due to  the use of xml config. And the clinicService is reused between calls.
+     *      So it makes the unique call of the captor gets called 2 time.
+     */
+    @AfterEach
+    void tearDown() {
+        reset(clinicService);
     }
 
     @Test
     void testFindByNameNotFound() throws Exception {
+
         mockMvc.perform(
                 get("/owners")
                         .param("lastName", "Dont find me"))
@@ -44,8 +64,40 @@ class OwnerControllerTest {
     }
 
     @Test
+    void testNameNull() throws Exception {
+        given(clinicService.findOwnerByLastName("")).willReturn(Arrays.asList(new Owner(), new Owner()));
+
+        mockMvc.perform(
+                get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("selections"))
+                .andExpect(view().name("owners/ownersList"));
+
+        // Unique call see comment.
+        then(clinicService).should().findOwnerByLastName(stringCaptor.capture());
+
+        assertThat(stringCaptor.getValue()).isEqualTo("");
+    }
+
+    @Test
+    void testOneResult() throws Exception {
+
+        // Given
+        Owner owner = new Owner();
+        owner.setId(100);
+        given(clinicService.findOwnerByLastName("one")).willReturn(Arrays.asList(owner));
+
+        // When then
+        mockMvc.perform(
+                get("/owners")
+                        .param("lastName", "one"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/owners/100"));
+    }
+
+    @Test
     void testInitCreationForm() throws Exception {
-        mockMvc.perform(get("/owners"))
+        mockMvc.perform(get("/owners/new"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(view().name(OwnerController.VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
