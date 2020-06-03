5fff5cd081a6776453bdb3a676a9fedbf9bad590
Jean-Francois Larouche <rouche@gmail.com>
2020-06-03 10:49:33 -0400

    Using Spring MVC Test with Form Parameters

--- tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,12 +1,17 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,18 +21,31 @@ class OwnerControllerTest {
     @Autowired
     private OwnerController ownerController;
 
+    @Autowired
+    private ClinicService clinicService;
+
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
+        given(clinicService.findOwnerByLastName(anyString())).willReturn(new ArrayList<>());
 
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
     @Test
-    void testInitCreationForm() throws Exception {
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(
+                get("/owners")
+                        .param("lastName", "Dont find me"))
+                .andExpect(status().isOk())
+                .andExpect(model().errorCount(1))
+                .andExpect(view().name("owners/findOwners"));
+    }
 
-        mockMvc.perform(get("/owners/new"))
+    @Test
+    void testInitCreationForm() throws Exception {
+        mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(view().name(OwnerController.VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
