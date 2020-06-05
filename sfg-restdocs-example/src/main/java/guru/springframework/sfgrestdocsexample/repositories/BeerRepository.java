4b5011363de0b71a1e2ea73e7eb57c7a403d6039
Jean-Francois Larouche <rouche@gmail.com>
2020-06-05 14:03:41 -0400

    Maven Configuration

---
+++ sfg-restdocs-example/src/main/java/guru/springframework/sfgrestdocsexample/repositories/BeerRepository.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgrestdocsexample.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+
+/**
+ * Created by jt on 2019-05-17.
+ */
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
